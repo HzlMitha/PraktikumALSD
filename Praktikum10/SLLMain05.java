6ccc82f5866d6b3edcdd558e1d484c8cfe5e9a81
Dian Paramitha <dianmitha89031@gmail.com>
2025-05-24 19:32:23 +0700

    Modifikasi SLLMain05

--- Praktikum10/SLLMain05.java
+++ Praktikum10/SLLMain05.java
@@ -1,33 +1,36 @@
 package Praktikum10;
+import java.util.Scanner;
 
 public class SLLMain05 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         SingleLinkedList05 sll = new SingleLinkedList05();
-        Mahasiswa05 mhs1 = new Mahasiswa05("24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa05 mhs2 = new Mahasiswa05("23213301", "Bimon", "2B", 3.8);
-        Mahasiswa05 mhs3 = new Mahasiswa05("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa05 mhs4 = new Mahasiswa05("21212203", "Dirga", "4D", 3.6);
+        
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jml = input.nextInt();
+        input.nextLine();
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+        for (int i = 0; i < jml; i++) {
+            System.out.println("=== Data Mahasiswa ke-" + (i+1) + " ===");
+            System.out.print("NIM    : ");
+            String nim = input.nextLine();
+            System.out.print("Nama   : ");
+            String nama = input.nextLine();
+            System.out.print("Kelas  : ");
+            String kelas = input.nextLine();
+            System.out.print("IPK    : ");
+            double ipk = input.nextDouble();
+            input.nextLine();
+
+            Mahasiswa05 mhs = new Mahasiswa05(nim, nama, kelas, ipk);
+            if (i == 0) {
+                sll.addFirst(mhs);
+            } else {
+                sll.addLast(mhs);
+            }
+        }
         System.out.println();
-
-        System.out.println("Data index 1 : ");
-        sll.getData(1);
-
-        System.out.println("Data mahasiswa an Bimon berada pada index : " + sll.indexOf("bimon"));
-        System.out.println();
-
-        sll.removeFirst();
-        sll.removeLast();
-        sll.print();
-        sll.removeAt(0);
+        System.out.println("=== List Mahasiswa ===");
         sll.print();
     }
 }
