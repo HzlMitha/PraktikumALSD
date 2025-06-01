94e8e349d0e0f003acd05ef9ff004558ea313d57
Dian Paramitha <dianmitha89031@gmail.com>
2025-06-01 18:48:52 +0700

    Tugas

--- Praktikum11/DLLMain.java
+++ Praktikum11/DLLMain.java
@@ -11,11 +11,14 @@ public class DLLMain {
             System.out.println("\nMenu Double Linked List Mahasiswa");
             System.out.println("1. Tambah di awal");
             System.out.println("2. Tambah di akhir");
-            System.out.println("3. Hapus di awal");
-            System.out.println("4. Hapus di akhir");
-            System.out.println("5. Tampilkan data");
-            System.out.println("6. Cari Mahasiswa berdasarkan NIM");
-            System.out.println("7. Tambah di tengah");
+            System.out.println("3. Tambah di tengah");
+            System.out.println("4. Hapus di awal");
+            System.out.println("5. Hapus di akhir");
+            System.out.println("6. Hapus di tengah");
+            System.out.println("7. Hapus berdasarkan indeks");
+            System.out.println("8. Tampilkan data");
+            System.out.println("9. Cari Mahasiswa berdasarkan NIM");
+            System.out.println("10. Tampilkan Jumlah data");
             System.out.println("0. Keluar");
             System.out.print("Pilih menu: ");
             pilihan = scan.nextInt();
@@ -30,10 +33,26 @@ public class DLLMain {
                     Mahasiswa05 mhs = inputMahasiswa(scan);
                     list.addLast(mhs);
                 }
-                case 3 -> list.removeFirst();
-                case 4 -> list.removeLast();
-                case 5 -> list.print();
+                case 3 -> {
+                    System.out.print("Masukkan key NIM yang ingin ditambahkan: ");
+                    String keyNim = scan.nextLine();
+                    Mahasiswa05 mhs = inputMahasiswa(scan);
+                    list.insertAfter(keyNim, mhs);
+                }
+                case 4 -> list.removeFirst();
+                case 5 -> list.removeLast();
                 case 6 -> {
+                    System.out.print("Masukkan key NIM yang ingin dihapus: ");
+                    String keyNim = scan.nextLine();
+                    list.removeAfter(keyNim);
+                }
+                case 7 -> {
+                    System.out.print("Masukkan index yang ingin dihapus: ");
+                    int index = scan.nextInt();
+                    list.remove(index);
+                }
+                case 8 -> list.print();
+                case 9 -> {
                     System.out.print("Masukkan NIM yang dicari: ");
                     String nim = scan.nextLine();
                     Node05 found = list.search(nim);
@@ -44,12 +63,7 @@ public class DLLMain {
                         System.out.println("Data tidak ditemukan.");
                     }
                 }
-                case 7 -> {
-                    System.out.print("Masukkan key NIM yang ingin ditambahkan: ");
-                    String keyNim = scan.nextLine();
-                    Mahasiswa05 mhs = inputMahasiswa(scan);
-                    list.insertAfter(keyNim, mhs);
-                }
+                case 10 -> System.out.println("Jumlah Data : " + list.size());
                 case 0 -> System.out.println("Keluar dari program.");
                 default -> System.out.println("Pilihan tidak valid!");
             }
--- Praktikum11/DoubleLinkedList05.java
+++ Praktikum11/DoubleLinkedList05.java
@@ -66,7 +66,72 @@ public class DoubleLinkedList05 {
         System.out.println("Node berhasil disisipkan setelah NIM " + keyNim);
     }
 
-    
+    public int size() {
+        int size = 0;
+        Node05 current = head;
+        while (current != null) {
+            size++;
+            current = current.next;
+        }
+        return size;
+    }
+
+    public void add(int index, Mahasiswa05 data) {
+        if (index < 0 || index > size()) {
+            System.out.println("Index tidak valid.");
+            return;
+        }
+        Node05 newNode = new Node05(data);
+
+        if (index == 0) {
+            addFirst(data);
+            return;
+        }
+
+        if (index == size()) {
+            addLast(data);
+            return;
+        }
+
+        Node05 current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        newNode.next = current.next;
+        newNode.prev = current;
+        current.next.prev = newNode;
+        current.next = newNode;
+    }
+
+    public Mahasiswa05 getFirst() {
+        if (isEmpty()) {
+            System.out.println("List masih kosong.");
+            return null;
+        }
+        return head.data;
+    }
+
+     public Mahasiswa05 getLast() {
+        if (isEmpty()) {
+            System.out.println("List masih kosong.");
+            return null;
+        }
+        return tail.data;
+    }
+
+    public Mahasiswa05 getIndex(int index) {
+        if (index < 0 || index >= size()) {
+            System.out.println("Index tidak valid");
+            return null;
+        }
+
+        Node05 current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return current.data;
+    }
 
     public void print() {
         if (isEmpty()) {
@@ -112,6 +177,51 @@ public class DoubleLinkedList05 {
         } else {
             tail = tail.prev;
             tail.next = null;
+        }
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size()) {
+            System.out.println("Index tidak valid.");
+            return;
+        }
+
+        if (index == 0) {
+            removeFirst();
+            return;
+        }
+
+        if (index == size() - 1) {
+            removeLast();
+            return;
+        }
+
+        Node05 current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        current.prev.next = current.next;
+        if (current.next != null) {
+            current.next.prev = current.prev;
+        }
+    }
+
+    public void removeAfter(String key) {
+        Node05 current = head;
+    
+        while (current != null && !current.data.nim.equals(key)) {
+            current = current.next;
+        }
+
+        if (current == null || current.next == null) {
+            System.out.println("Node setelah NIM " + key + " tidak ditemukan.");
+            return;
+        }
+
+        current.next = current.next.next;
+        if (current.next != null) {
+            current.next.prev = current;
         }
     }
 
